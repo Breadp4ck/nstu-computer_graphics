7f4de5b20fb62b0e0f6032f25d6f3b63dfe0b351
Breadp4ck <iii103@mail.ru>
2022-10-07 14:06:39 +0700

    Remove old comments.

--- lab1/App/FlyCamera.cs
+++ lab1/App/FlyCamera.cs
@@ -9,9 +9,6 @@ namespace Lab1.App
     {
         private Vector2 cameraRotation = new Vector2((float)(-System.Math.PI / 2.0f), 0.0f);
 
-        // private float pitch = 0.0f;
-        // private float yaw = 0.0f;
-
         public float Speed { get; private set; } = 10.0f;
         public FlyCamera3D(string name) : base(name) { }
 
@@ -57,35 +54,10 @@ namespace Lab1.App
                 (float)(System.Math.Sin(cameraRotation.X) * System.Math.Cos(cameraRotation.Y))
             );
 
-            // var rotationX = Quaternion.CreateFromAxisAngle(Vector3.UnitY, cameraRotation.X);
-            // var rotationY = Quaternion.CreateFromAxisAngle(Vector3.UnitX, cameraRotation.Y);
-
-            // Transform.Rotation = Quaternion.Concatenate(rotationX, rotationY);
-
             if (direction != Vector3.Zero)
             {
                 direction = Vector3.Normalize(direction);
-                // var angle = cameraRotation.X;
-
-                // direction = new Vector3(
-                //     (float)(direction.X * System.Math.Cos(-angle) + direction.Z * System.Math.Sin(-angle)),
-                //     direction.Y,
-                //     (float)(direction.Z * System.Math.Cos(-angle) - direction.X * System.Math.Sin(-angle))
-                // );
-
                 Translate(direction * delta * Speed);
-
-                // var lol = new MeshInstance3D("Куб");
-                // lol.MeshData = new CubePrimitive();
-
-                // var kek = new StandartMaterialResource();
-                // kek.Color = new Color(0.1f, 0.2f, 0.7f);
-                // lol.MaterialResource = kek;
-
-                // Parent!.AddChild(lol);
-
-                // lol.Transform = GlobalTransform;
-                // lol.Transform.Position = GlobalTransform.Position - Vector3.UnitY * 3.0f;
             }
         }
 
